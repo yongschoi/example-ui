2321743f11a5e9b2b98adee031199c8725493cb3
yongschoi <yongschoi@naver.com>
2020-10-26 08:42:35 +0900

    api host 변경(10.0.2.15:7071)

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
-Vue.prototype.$apiURI = "http://127.0.0.1:7071";
+Vue.prototype.$apiURI = "http://10.0.2.15:7071";
 
 new Vue({
   router,
