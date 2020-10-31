738f10045d5bd67e303f49ad5607f01ab6057f7f
yongschoi <yongschoi@naver.com>
2020-10-31 13:36:58 +0900

    127.0.0.1 -> localhost로 변경

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import router from "./router";
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
-Vue.prototype.$userAPIUri = "http://127.0.0.1:8081";
-Vue.prototype.$productAPIUri = "http://127.0.0.1:8082";
+Vue.prototype.$userAPIUri = "http://localhost:8081";
+Vue.prototype.$productAPIUri = "http://localhost:8082";
 
 new Vue({
   router,
